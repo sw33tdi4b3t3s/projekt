c97e95c429c8ca61481ff5c52dfbc0d6c506968d
sw33tdi4b3t3s <marchewka.wiktor@gmail.com>
2026-06-24 20:35:44 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req,res)=>{
+
+    res.send("Hello world klient");
+    console.log("Hello World");
+
+});
+
+
+app.listen(3000,()=>{
+
+    console.log("aplikacja dziala!");
+
+});
